4abbb2e296002f6b78cb63a698b725ca7db6fbbc
Ebube Anyanwu <ebubechi.anyanwu@yahoo.com>
2024-04-03 08:31:36 +0100

    added images import

--- portfolio_frontend/src/components/Navbar/Navbar.jsx
+++ portfolio_frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
+import { images } from '../../constants'
 import './Navbar.scss'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <nav>
+            <div>
+                <img src={images.logoWhite} alt='logo' />
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
